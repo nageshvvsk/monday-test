c69d5bf1e3a38c3b6a385cef539df2a055062b5d
Nagesh KVVS <nagesh.k@inteqsolutions.com>
2021-05-31 17:37:09 +0530

    mod again

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <p>nagesh</p>
+      <p>nagesh modified</p>
       <p>{new Date().toLocaleString() + ""}</p>
       <h1>Herambha</h1>
     </div>
